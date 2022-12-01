2c52de7f5ea336416eb06b504dc52fe1418da6f2
MercedesUbeira <mercedesubeir@gmail.com>
2022-12-01 16:25:06 +0100

    Flight booking calculator

---
+++ javascript/javascript1/week2/flight.js
@@ -0,0 +1,26 @@
+//Flight 
+const firstname= "Mercedes"
+const surname= "Ubeira"
+
+function getFullName(firstname, surname) {
+  console.log (firstname + surname);
+}
+
+getFullName (firstname,surname);
+
+let fullname1= "Juan Perez";
+let fullname2= " Lionel Messi";
+
+getFullName (fullname1 , fullname2);
+
+const userFormalName = true
+const gender= "female"
+  if (userFormalName && gender === "male") {
+    console.log (`Lord ${firstname} ${surname}`);
+  } else if  (userFormalName && gender === "female" ) {
+    console.log (`Lady ${firstname} ${surname}`);
+  } else if (firstname === "" || firstname === Number) {
+    console.log ("Try again.");
+  } else if (surname === "" || surname === Number) {
+  console.log ("Try again.");
+  } else { console.log ("Unexpected error. Try again.")}
