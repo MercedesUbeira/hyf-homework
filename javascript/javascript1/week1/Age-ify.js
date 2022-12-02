2f6233f0ffe70011df403e760f7dbb37be0c8134
MercedesUbeira <115772453+MercedesUbeira@users.noreply.github.com>
2022-12-02 16:50:28 +0100

    Update Age-ify.js

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -1,7 +1,7 @@
 //Age-ify (A future age calculator)
 
 const yearOfBirth = 1998;
-let yearFuture = 2045;
+const yearFuture = 2045;
 const difference = yearFuture - yearOfBirth;
 
 console.log("You will be " + difference + " in " + yearFuture);
